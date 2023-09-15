36824098fcfa11773b5101a97724fde3134f9ef6
herickaquino1 <rherickaquino18@gmail.com>
2023-09-15 12:29:26 -0300

    Desafio de controle de fluxo da Dio, bootcamp santander java

---
+++ src/Desafio/Contador.java
@@ -0,0 +1,36 @@
+package Desafio;
+
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner sca = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = sca.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = sca.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroDois < parametroUm) {
+			throw new ParametrosInvalidosException();
+			
+		}else {
+			int contagem = parametroDois - parametroUm;
+			for(int indice = 1; indice <= contagem; indice++) {
+				System.out.println("imprimindo numero: "+indice);
+			}
+		}
+		
+		
+	}
+
+}
